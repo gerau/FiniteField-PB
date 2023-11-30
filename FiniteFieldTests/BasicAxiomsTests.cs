f438bf6d8dbfa69d7bf5937fcbfe5ffc2de56146
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-11-30 16:50:40 +0200

    added basic axioms tests

---
+++ FiniteFieldTests/BasicAxiomsTests.cs
@@ -0,0 +1,74 @@
+using FiniteField.GaloisField;
+using FiniteField.Helpers;
+
+
+namespace FiniteFieldTests
+{
+    public class BasicAxiomsTests
+    {
+        public Element A;
+        public Element B;
+        public Element C;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            A = Generator.Generate();
+            B = Generator.Generate();
+            C = Generator.Generate();
+        }
+
+        [Test]
+        public void RightDistributivity()
+        {
+            Element X = (A + B) * C;
+            Element Y = A * C + B * C;
+
+            Assert.That(X == Y);
+        }
+
+        [Test]
+        public void LeftDistributivity()
+        {
+            Element X = C * (A + B);
+            Element Y = C * A + C * B;
+
+            Assert.That(X == Y);
+        }
+
+        [Test]
+        public void CommitativityAdd()
+        {
+            Element X = A + B;
+            Element Y = B + A;
+
+            Assert.That(X == Y);
+        }
+
+        [Test]
+        public void CommitativityMult()
+        {
+            Element X = A * B;
+            Element Y = B * A;
+
+            Assert.That(X == Y);
+        }
+        [Test]
+        public void AssociativityAdd()
+        {
+            Element X = (A + B) + C;
+            Element Y = A + (B + C);
+
+            Assert.That(X == Y);
+        }
+        [Test]
+        public void AssociativityMult()
+        {
+            Element X = (A * B) * C;
+            Element Y = A * (B * C);
+
+            Assert.That(X == Y);
+        }
+    }
+}
